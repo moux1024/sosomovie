e936108b4992ba0f7b222c82ee1edc7b8c80a053
xueteng <xueteng@quarkioe.com>
2018-01-21 14:10:27 +0800

    first push

---
+++ src/components/Home.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div class="home">
+      <!-- <ISection :movieObject="movieObject"></ISection> -->
+      <div class="">
+
+      </div>
+      <ISection v-for="movieObject in testData">
+      </ISection>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import ISection from './Home/ISection';
+console.log(window);
+export default {
+  name:"home",
+  mounted() {
+    //TODO: loading图
+  }, //页面加载完成
+  components: {
+    ISection
+  },
+  data () {
+    return {
+      count: 0,
+      classObject: "w0",
+      testData: [{
+          img: "static/img/01.jpg",
+          textEn: "Apple",
+          textCn: "芬恩芬恩"
+        },
+        {
+          img: "static/img/02.jpg",
+          textEn: "Car",
+          textCn: "芬恩芬恩"
+        },
+        {
+          img: "static/img/03.jpg",
+          textEn: "pangxie",
+          textCn: "芬恩芬恩"
+        },
+        {
+          img: "static/img/04.jpg",
+          textEn: "vans",
+          textCn: "芬恩芬恩"
+        },
+        {
+          img: "static/img/05.jpg",
+          textEn: "alipay",
+          textCn: "芬恩芬恩"
+        }
+      ],
+      movieList: [
+        {
+          name:"test",
+          url:"www.baidu.com"
+        }
+      ],
+      BURL:BURL
+    }
+  },
+  methods: {
+    more(id) {
+      this.$router.push("/detail/" + id)
+    },
+    load() {
+      var vm = this;
+      console.log(window);
+      axios.get(BURL + ART + "querylist.do").then(function(res) {
+        vm.movieList = res.data.list
+        console.log(vm.movieList);
+      })
+    },
+    loadMore: function() {
+      this.busy = true;
+      var vm = this;
+      axios.get(BURL + ART + "querylist.do").then(function(res) {
+        vm.movieList = vm.movieList.concat(res.data.list)
+        console.log(vm.movieList);
+        vm.busy = false;
+      })
+    }
+  }
+}
+</script>
+
+<style>
+/* .vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+} */
+</style>
---
+++ src/components/Home/ISection.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+<div class="">
+  123
+  <!-- <div class="container" @click="more(data.basicId)"> -->
+  <div class="container" @click="">
+      <img :src="BURL+data.basicPic" :alt="data.basicTitle">
+      <div class="text-container">
+        <div class="panel">
+          <div class="panel-img">
+            <img :src="BURL+data.basicPic" :alt="data.basicTitle">
+          </div>
+          <div class="panel-text">
+            <p class="title-cn">{{data.articleName}}</p>
+            <p class="title-en">{{data.articleEngName}}</p>
+            <p class="director">导演：{{data.articleSource}}</p>
+            <p class="stars">
+
+              <img class="imdbmark" src="static/img/IMDb@2x.png">
+            </p>
+            <p class="tips">
+              <span>{{data.movieYear}}</span>
+              <span>{{data.movieCountry||"国家未知"}}</span>
+              <span>{{data.movieType||"类型未知"}}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+console.log(window);
+export default {
+  name:'ISection',
+  props:[
+    'movieObject'
+  ],
+  data () {
+    return {
+      msg: 'this is ISection!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
